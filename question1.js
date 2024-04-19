891fbb5d290304e624d69c2f7303c7e187bbbd0e
HarshiniM18 <154896188+HarshiniM18@users.noreply.github.com>
2024-04-19 17:40:49 +0530

    question1.js

--- question1.js
+++ question1.js
@@ -1,26 +1,33 @@
-class Movie {
-    constructor(title, studio, rating = "PG") {
-      this.title = title;
-      this.studio = studio;
-      this.rating = rating;
+//1. An instance of class Movie represents a film
+
+class Movie{
+    constructor(title,studio,rating = "PG"){
+        this.title = title;
+        this.studio = studio;
+        this.rating = rating;
     }
-  
-    static getPG(movies) {
-      return movies.filter(movie => movie.rating === "PG");
+    static getPG(movies){
+        return movies.filter(movie => movie.rating === "PG");
     }
-  }
-  
-  const casinoRoyale = new Movie("Casino Royale", "Eon Productions", "PG-13");
-  
-  const moviesArray = [
-    new Movie("Movie1", "Studio1", "PG"),
-    new Movie("Movie2", "Studio2", "PG-13"),
-    new Movie("Movie3", "Studio3", "PG"),
-    new Movie("Movie4", "Studio4", "R"),
-  ];
-  
-  const pgMovies = Movie.getPG(moviesArray);
-  
-  console.log(casinoRoyale);
-  console.log(pgMovies);
-  
+}
+
+// Creating an instance of Movie with the given details
+const casinoRoyale = new Movie("Casino Royale","Eon Productions","PG-13");
+
+//Example usage
+const movies = [
+    new Movie("Movie 1","Studio A","PG"),
+    new Movie("Movie 2","Studio B","PG-13"),
+    new Movie("Movie 3","Studio C","R"),
+    new Movie("Movie 4","Studio D") // This movie will have default rating "PG"
+];
+
+const pgMovies = Movie.getPG(movies);
+console.log(pgMovies);
+
+
+//output:
+[
+  Movie { title: 'Movie 1', studio: 'Studio A', rating: 'PG' },
+  Movie { title: 'Movie 4', studio: 'Studio D', rating: 'PG' }
+]
